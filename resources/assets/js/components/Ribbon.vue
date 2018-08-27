19c1aa0b903941ad4a6aab183c65be2f65e04c1a
Sumanta Kr Ghosh <sumanta.ghosh.mit@gmail.com>
2018-08-27 22:18:01 +0530

    Create a ribbon component.

---
+++ resources/assets/js/components/Ribbon.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="ribbon" :class="tag">
+        <div class="theribbon">{{label}}</div>
+        <div class="ribbon-background"></div>
+    </div>
+</template>
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    tag: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
